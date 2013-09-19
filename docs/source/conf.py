74b0e02b2269e266772886ce9ebcd842e8d4fa9a
José Luis Lafuente <jlesquembre@gmail.com>
2013-09-19 14:34:13 +0200

    Mock imports for modules with C dependencies in the docs for read the docs compatibility.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,32 @@ import pkg_resources
 sys.path.insert(0, os.path.abspath('../..'))
 #sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+
+# -- Read the docs options ------------------
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['magic']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -97,7 +123,27 @@ pygments_style = 'sphinx'
 
 # If true, keep warnings as "system message" paragraphs in the built documents.
 #keep_warnings = False
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
 
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
 
 # -- Options for HTML output ---------------------------------------------------
 
