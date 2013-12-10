eddcea58ea57f435610de49548a08fffb0c30234
Jose Luis Lafuente <jlesquembre@gmail.com>
2013-12-10 23:33:41 +0100

    Back to development: 0.1.6

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,12 @@
 Changelog for pytag
 ===================
 
+0.1.6 (unreleased)
+------------------
+
+- Nothing changed yet.
+
+
 0.1.5 (2013-12-10)
 ------------------
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), 'rt') as f:
     README = f.read()
 
-version = '0.1.5'
+version = '0.1.6.dev0'
 
 setup(name='pytag',
       version=version,
